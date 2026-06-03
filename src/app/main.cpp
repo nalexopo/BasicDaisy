3dab4e7c92dd8e8c0ea59962b51ec55a7c3b3c1d
Nikos Alexo <nalexopo@gmail.com>
2026-06-03 22:33:07 +0300

    Restore main to Delay

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,16 +1,13 @@
 #include "main.h"
 #include "DigitalDelay.h"
-#include "lfo.h"
 #include "adc.h"
 #include "stdatomic.h"
 
 #define DELAY_SAMPLE_DEPTH 480000U
 DaisySeed Board;
 
-lfo TestSin(48000.0f);
-//DSY_SDRAM_BSS float Buffer[48000];
+DigitalDelay<float, DELAY_SAMPLE_DEPTH> DSY_SDRAM_BSS DDelay;
 int i=0;
-int j=0;
 std::atomic<bool> UpdateSettings = false;
 
 void AudioCallback(AudioHandle::InputBuffer  in,
@@ -20,15 +17,14 @@ void AudioCallback(AudioHandle::InputBuffer  in,
     if(UpdateSettings)
     {
         UpdateSettings = false;
-
+        DDelay.AdjustMix(Board.adc.GetFloat(0));
+        DDelay.AdjustFeedback(Board.adc.GetFloat(1));
+        DDelay.AdjustDelay(Board.adc.GetFloat(2));
     }    
     for(size_t i = 0; i < size; i++)
     {
         /** Set each of our outputs to the value of this sine wave */
-        OUT_L[i] = TestSin.Process();
-        // Buffer[j] = OUT_L[i];
-        // j++;
-        // if(j==48000)j=0;
+        OUT_L[i] = DDelay.Process(IN_L[i]);
         //OUT_L[i] = IN_L[i];
         //OUT_R[i] = IN_R[i];
     }
@@ -39,6 +35,16 @@ int main(void)
    
     Board.Init();
     
+    DDelay.Reset(1.5f, 0.8f, 0.7f, 32000);
+    //ADC Init
+    AdcChannelConfig adcConfig[4];
+    adcConfig[0].InitSingle(Board.GetPin(16));
+    adcConfig[1].InitSingle(Board.GetPin(17));
+    adcConfig[2].InitSingle(Board.GetPin(18));
+    adcConfig[3].InitSingle(Board.GetPin(19));
+  
+    Board.adc.Init(adcConfig, 4);
+    Board.adc.Start();
     
     Board.StartAudio(AudioCallback);
 
